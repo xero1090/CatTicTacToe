42c1c5031b83aa8542f3cd4a9de6a750254550fa
Kevin Tran <tran94@sheridancollege.ca>
2020-01-23 17:55:48 -0500

    Added a textblock for mainpage

--- Interactive Memory Game/Interactive Memory Game/MainPage.xaml.cs
+++ Interactive Memory Game/Interactive Memory Game/MainPage.xaml.cs
@@ -20,9 +20,9 @@ namespace Interactive_Memory_Game
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class MainPage : Page
     {
-        public BlankPage1()
+        public MainPage()
         {
             this.InitializeComponent();
         }
