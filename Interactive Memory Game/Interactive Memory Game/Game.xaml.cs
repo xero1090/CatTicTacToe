cb9d92bac94d43eaf62b9421f69c1c2d79e81cae
Kevin Tran <tran94@sheridancollege.ca>
2020-01-23 17:18:34 -0500

    Created a second page and renamed MainPage.xaml to Game.xaml

--- Interactive Memory Game/Interactive Memory Game/Game.xaml.cs
+++ Interactive Memory Game/Interactive Memory Game/Game.xaml.cs
@@ -13,16 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Interactive_Memory_Game
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class MainPage : Page
     {
-        public BlankPage1()
+        public MainPage()
         {
             this.InitializeComponent();
         }
