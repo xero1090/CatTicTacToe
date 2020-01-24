2b64c343129a3624ee61445817bf6ca6c4512616
Kevin Tran <tran94@sheridancollege.ca>
2020-01-23 23:23:18 -0500

    Added a "surprise" and  functionality to Mainpage button. Shhhh!

--- Interactive Memory Game/Interactive Memory Game/MainPage.xaml.cs
+++ Interactive Memory Game/Interactive Memory Game/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Media.Playback;
+using Windows.Media.Core;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,14 +24,27 @@ namespace Interactive_Memory_Game
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // Declare our Windows Media Player
+        MediaPlayer player;
+
         public MainPage()
         {
             this.InitializeComponent();
+            player = new MediaPlayer();
         }
 
-        private void OnClick(object sender, RoutedEventArgs e)
+        private async void OnClick(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(Game));
+            
+            // Searches within the Assets Folder
+            Windows.Storage.StorageFolder folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"Assets");
+
+            // Searches for specific file
+            Windows.Storage.StorageFile file = await folder.GetFileAsync("Dog of Wisdom Remix.mp3");
+
+            player.Source = MediaSource.CreateFromStorageFile(file);
+            player.Play();
         }
 
         private void ToEnd(object sender, RoutedEventArgs e)
